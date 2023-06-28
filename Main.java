8dec7e9867979ab82e99534e39b08fc3c84f8038
Mountblack8185 <135257493+Mountblack8185@users.noreply.github.com>
2023-06-28 23:05:40 +0000

    Attempt 7

--- Main.java
+++ Main.java
@@ -15,11 +15,11 @@ class Main {
 
     do
     {
-      System.out.println("press 1 to learn about salary.");
-      System.out.println("press 2 to learn about the job.");
-      System.out.println("press 3 to learn about demand.");
-      System.out.println("press 4 to view current students");
-      System.out.println("press 5 to quit");
+      System.out.println("Press 1 to learn about salary.");
+      System.out.println("Press 2 to learn about the job.");
+      System.out.println("Press 3 to learn about demand.");
+      System.out.println("Press 4 to view current students");
+      System.out.println("Press 5 to quit");
       UserNum = option.nextInt();
       
       if(UserNum == 1)
